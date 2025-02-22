c771b12eca085127ce962e376700d1fa24ae6741
SimplyXtra (Laptop) <bashirthaha@gmail.com>
2025-02-22 18:20:24 +1100

    Added Enemy AI
    Simple Goomba AI

---
+++ Scenes/rock_enemy.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+#Variables
+var speed:float = 50
+var direction_x:float = 1
+
+#Runs every frame
+func _physics_process(delta: float) -> void:
+	#Redirect direction on collision
+	if is_on_wall():
+		direction_x *= -1
+	
+	#Add Gravity
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	#Move
+	velocity.x = speed * direction_x
+	move_and_slide()
+	
